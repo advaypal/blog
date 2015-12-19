17ef1706822ce8b5f95d696f6cfffd7f2738c114
Advay Pal <advaypal@hotmail.com>
2015-12-19 15:55:32 +0530

    First commit

---
+++ src/views/homepage.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/static/css/bootstrap.css"/>
+		<link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
+	</head>
+	<body>
+		<div class="nav">
+			<div class="container">
+				<div class="pull-left">
+					<ul class="nav nav-pills">
+					  <li class="active"><a href="#">Home</a></li>
+					  <li><a href="#"> </a></li>
+  					  <li><a href="#"> </a></li>
+					</ul>
+				</div>
+				<div class="pull-right">
+					<div class="form-inline">
+						<input class ="form-control" type="text"; placeholder="Email";/>
+						<input class ="form-control" type="text"; placeholder="Password"; />
+						<a href="user.html">
+							<button type="button" class="btn btn-default"> Login</button>
+						</a>
+					</div>
+				</div>	
+			</div>
+		</div>
+		<div class="Header">
+			<div class="container">
+				<h1 class="text-center"> Untold Stories </h1>
+			</div>
+        </div>
+        <div class="search">
+        	<div class ="container">
+        		<p> Search this blog</p>
+        		<div class="form-inline">
+        			<input class ="form-control" type="text";/>
+        		</div>
+        		<p> </p>
+        	    <button type="button" class="btn btn-default"> Search </button>
+        	</div>
+        </div>
+        <div class="posts">'
+        	
+	    </div>
+	</body>
+</html>
