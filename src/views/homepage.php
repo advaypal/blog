44fb9a0259cb3aa4c45e7d7c3295bd09ecaba817
Advay Pal <advaypal@hotmail.com>
2016-01-07 11:45:07 +0530

    First

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -1,46 +1,39 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="/static/css/bootstrap.css"/>
-		<link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
-	</head>
-	<body>
-		<div class="nav">
-			<div class="container">
-				<div class="pull-left">
-					<ul class="nav nav-pills">
-					  <li class="active"><a href="#">Home</a></li>
-					  <li><a href="#"> </a></li>
-  					  <li><a href="#"> </a></li>
-					</ul>
-				</div>
-				<div class="pull-right">
-					<div class="form-inline">
-						<input class ="form-control" type="text"; placeholder="Email";/>
-						<input class ="form-control" type="text"; placeholder="Password"; />
-						<a href="user.html">
-							<button type="button" class="btn btn-default"> Login</button>
-						</a>
-					</div>
-				</div>	
-			</div>
+<head>
+	<script type="text/javascript">
+		//acts as a confirmation alert
+		function deleteconfirm() {
+			return confirm("Are you sure you wish to delete this post?");
+		}
+	</script>
+</head>
+<body>
+	<?php
+		require_once("common/header.php");
+	?>
+	<div class = "container">
+	<div class = "Header">
+		<div class = "container">
+			<h1 class="text-center"> Advay's blog </h1>
 		</div>
-		<div class="Header">
-			<div class="container">
-				<h1 class="text-center"> Untold Stories </h1>
-			</div>
-        </div>
-        <div class="search">
-        	<div class ="container">
-        		<p> Search this blog</p>
-        		<div class="form-inline">
-        			<input class ="form-control" type="text";/>
-        		</div>
-        		<p> </p>
-        	    <button type="button" class="btn btn-default"> Search </button>
-        	</div>
-        </div>
-        <div class="posts">'
-        	
-	    </div>
-	</body>
+	</div>
+	</div>
+	<?php
+		//display message
+		if(isset($_GET["message"]) && (!isset($message) || $message=="")) {
+			$msg = $_GET["message"];
+			echo '
+				<div class="container">
+					<div class="col-md-10">
+						<h4> '.$msg.'</h4>
+					</div>
+				</div>
+			';
+		}
+		//filter out unnecessary posts
+		$filter = function($id, $userid, $author, $title) { return true; };
+		require_once("../db/retrieve_posts.php");
+	?>
+</body>
 </html>
